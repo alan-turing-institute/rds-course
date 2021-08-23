916db8803d3edd0836ea74588528e5bafbaad6e2
ChristinaLast <christina.last@outlook.com>
2021-08-23 17:04:04 +0100

    :lipstick: adding code formatting to countplot

--- data-analysis/scripts/eqls_count_plot.py
+++ data-analysis/scripts/eqls_count_plot.py
@@ -1,4 +1,3 @@
-
 # import bootstrap  # noqa
 import os
 import plotly.express as px
@@ -6,6 +5,7 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from textwrap import wrap
 from categorical_variable_mapping import CategoricalDataConfig
 
 categorical_data_config = CategoricalDataConfig()
@@ -28,21 +28,28 @@ eqls_df["Y11_Agecategory_cat"] = eqls_df["Y11_Agecategory"].apply(lambda x: cate
 eqls_df["Y11_HH2a_cat"] = eqls_df["Y11_HH2a"].apply(lambda x: categorical_data_config.Y11_HH2a.get(x))
 eqls_df["Y11_HHsize_cat"] = eqls_df["Y11_HHsize"].apply(lambda x: categorical_data_config.Y11_HHsize.get(x))
 
-
 categorical = [
   'Y11_Country_cat', 'Y11_Q31_cat', 'Y11_ISCEDsimple_cat', 'Y11_Q49_cat', 'Y11_Agecategory_cat', 'Y11_HH2a_cat', 'Y11_HHsize_cat'
 ]
 
-fig, ax = plt.subplots(2, 3, figsize=(20, 10))
+fig, ax = plt.subplots(2, 3, figsize=(35, 10))
+fig.subplots_adjust(hspace=0.8)
 
 for variable, subplot in zip(categorical, ax.flatten()):
     sns.countplot(eqls_df[variable], ax=subplot)
+    labels= []
     for label in subplot.get_xticklabels():
+        label.set_text(label)
+        if len(label.get_text()) > 20:
+            label_formatted = '\n'.join(wrap(label.get_text(), 20))
+            label.set_text(label_formatted)
         label.set_rotation(90)
+        labels.append(label)
+    subplot.set_xticklabels(labels)
 
 plt.xlabel("Self-reported health")
 plt.ylabel("Count")
 # add title
-plt.title('Histogram of european self-reported health in 2011')
+plt.title('Countplot of european self-reported health in 2011')
 plt.legend(bbox_to_anchor=(.80, 0.8), loc=2);
 plt.show()
